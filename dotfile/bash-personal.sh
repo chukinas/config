da2425cce28b939f95752a993f69a4bee13aa7d5
Jonathan Chukinas (system76) <chukinas@gmail.com>
2021-11-29 19:37:02 -0500

    ignore doc directory

--- dotfile/bash-personal.sh
+++ dotfile/bash-personal.sh
@@ -4,7 +4,9 @@
 # ~/projects/dotfiles/dotfile/bash-personal.sh
 # ~/projects/dotfiles/dotfile/bash-comoto.sh
 
-alias dr="cd ~/projects/chukinas"
+alias cdg="cd ~/projects/gratuitous_greyhound"
+alias cdsu="cd ~/projects/gratuitous_greyhound/apps/suns_core"
+alias cdst="cd ~/projects/gratuitous_greyhound/apps/statechart"
 
 # Load private env vars
 source ~/.bashrc_private 2>/dev/null
